cc8279a08ac87776c3272d7137615d13210f2432
Naru11 <2925315190@qq.com>
2024-01-22 19:38:01 +0800

    轮播图

---
+++ src/views/Home/components/HomeBanner.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { getBannerAPI } from '@/apis/home'
+import {ref,onMounted} from 'vue'
+
+const BannerList = ref([])
+const getBanner= async ()=> {
+    const res = await getBannerAPI()
+    BannerList.value = res.result;
+    console.log(BannerList)
+    
+}
+
+onMounted(() => {
+    getBanner()
+})
+</script>
+
+
+
+<template>
+    <div class="home-banner">
+        <el-carousel height="500px">
+            <el-carousel-item v-for="item in BannerList" :key="item.id">
+                <img :src="item.imgUrl"
+                    alt="">
+            </el-carousel-item>
+        </el-carousel>
+    </div>
+</template>
+
+
+
+<style scoped lang='scss'>
+.home-banner {
+    width: 1240px;
+    height: 500px;
+    position: absolute;
+    left: 0;
+    top: 0;
+    z-index: 98;
+
+    img {
+        width: 100%;
+        height: 500px;
+    }
+}
+</style>
