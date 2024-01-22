287a591bd08777043ee787b4917c634863628a3c
Naru11 <2925315190@qq.com>
2024-01-22 17:16:33 +0800

    pinia导航数据

--- src/main.js
+++ src/main.js
@@ -5,8 +5,12 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+import '@/styles/common.scss'
+
 const app = createApp(App)
+
+
+
 
 app.use(createPinia())
 app.use(router)
